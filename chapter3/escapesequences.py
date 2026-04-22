739cd3845566009d5dd7bcaefc936ce49f46c98d
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 19:46:23 +0530

    filenamechanged

---
+++ chapter3/escapesequences.py
@@ -0,0 +1,5 @@
+name="hey hello this is this and \n that is \t that"
+print(name)
+name1="hey this is rabbani \"Good boy is rabbani\""
+print(name1)
+print("\u2764")
---
+++ chapter3/string_functions.py
@@ -0,0 +1,12 @@
+name="rabbani"
+# a=29.8
+print(len(name))
+print(name.startswith("ra"))
+
+print(name.endswith("nu"))
+
+print(name.capitalize())
+print(name.replace("rabbani","hello"))
+print(name.find("rabbani"))
+print(name.isdigit())
+print(name.isalpha())
