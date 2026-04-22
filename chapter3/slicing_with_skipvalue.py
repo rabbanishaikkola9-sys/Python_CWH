bc0663c97ecabb04b8f8de525bd0e5da4ef6d8cc
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 17:01:22 +0530

    Update slicing_with_skipvalue.py

--- chapter3/slicing_with_skipvalue.py
+++ chapter3/slicing_with_skipvalue.py
@@ -1,6 +1,7 @@
 number="0123456789"
-alphabet="abcdefgh"
+alphabet="abcdefghijklmnopqrstuvwxyz"
 print(number[1:7:3])
 print(alphabet[1:7:3])
 print(alphabet[1:9:2])
 print(alphabet[1:9:3])
+print(alphabet[0:25])
