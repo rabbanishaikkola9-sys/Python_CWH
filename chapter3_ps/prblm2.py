452913aa65e6decdc22cce62c147281d82948b33
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 19:54:58 +0530

    file added

---
+++ chapter3_ps/prblm2.py
@@ -0,0 +1,10 @@
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+name=input("Enter the username:\n")
+print("Dear " ,name,"\n You are selected!\n","22/04/2026")  
+# Can also use the f strings
+print(f"Good afternoon {name}\n You are selected \n 22/04/2026")
+print("End of the program:....")
