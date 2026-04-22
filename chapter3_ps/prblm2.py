f431cfac129c847c749c5960e81dbd9b8752c7f9
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 19:57:57 +0530

    Update prblm2.py

--- chapter3_ps/prblm2.py
+++ chapter3_ps/prblm2.py
@@ -8,3 +8,10 @@ print("Dear " ,name,"\n You are selected!\n","22/04/2026")
 # Can also use the f strings
 print(f"Good afternoon {name}\n You are selected \n 22/04/2026")
 print("End of the program:....")
+#  we can use it like this 
+letter = '''
+Dear <|Name|>,
+You are selected!
+<|Date|>
+'''
+print(letter.replace ("<|Name",name).replace("<|Date|>","22/04/2026"))
