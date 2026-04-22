9b555aeaff41ab8e614d88b77daeaa310a841d35
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-22 21:45:50 +0530

    file added

--- chapter3_ps/prblm4.py
+++ chapter3_ps/prblm4.py
@@ -1,2 +1,3 @@
 name="Rabbani is a good  boy"
 print(name.replace("  ","?")) #Double space is replaced with the ? or any string that you enter there as a parameter
+print(name) # strings are immutable which means you cannot change by running the prgms or methods 
