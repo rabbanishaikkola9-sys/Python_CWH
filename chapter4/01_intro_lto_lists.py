8414fba438b1b025201986933cd2242f3cb33b6e
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-23 10:29:52 +0530

    Create 01_intro_lto_lists.py

---
+++ chapter4/01_intro_lto_lists.py
@@ -0,0 +1,5 @@
+li=["APPLE",True,25,29.5,None,"Rabbani","The king"]
+print("The list of the items are :\n",li)
+# List is a container which stores every type of data and it can be changed hence, the lists are mutable by nature meaning that we can change the value acc to the indexes by assigning new values to them
+li[0]="Mango"
+print("The list of the items are:\n",li)
