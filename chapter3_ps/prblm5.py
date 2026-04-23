a84a9de57b1a45396c589c73f260152a20cd6f42
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-23 09:52:42 +0530

    Update prblm5.py

--- chapter3_ps/prblm5.py
+++ chapter3_ps/prblm5.py
@@ -1,2 +1,3 @@
 letter="Dear Harry,\n \tThis python course is nice.\n\t Thanks!"
 print(letter)
+print(f"hello!! {letter}") #using f string in python f means format the string we can use the variables without concatination just by using the variables with in the curly braces{}
