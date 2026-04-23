9864b44f5e3c24d8dc4606e4b33ba73ccba972fd
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-23 20:13:33 +0530

    fd

---
+++ dictionary&sets/03_sets.py
@@ -0,0 +1,8 @@
+s={1,2,3}
+print(s,type(s))
+# creating an empty set
+s=set()
+print(s,type(s))
+# Set doesnot allow the duplicate values
+my_set={11,22,11,22,1,2,3,4,5}
+print(my_set,type(my_set))
---
+++ dictionary&sets/sets-methods.py
@@ -0,0 +1,2 @@
+my_set={11,22,11,22,1,2,3,4,5}
+print(my_set,type(my_set))
