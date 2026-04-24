f7a0a6850369b08ae57d2627586272eff0ca028f
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-04-24 11:04:53 +0530

    files+_+

---
+++ dictionary&sets_ps/prblm6.py
@@ -0,0 +1,18 @@
+d={}
+name=input("Enter your user name")
+lang=input("Enter the fav lang")
+d.update({name:lang})
+
+name=input("Enter your user name")
+lang=input("Enter the fav lang")
+d.update({name:lang})
+
+name=input("Enter your user name")
+lang=input("Enter the fav lang")
+d.update({name:lang})
+
+name=input("Enter your user name")
+lang=input("Enter the fav lang")
+d.update({name:lang})
+
+print(d,type(d))
