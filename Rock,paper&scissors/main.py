db5f67d68fb06e6a664bb18e2928f20aac8890e5
KOLA MOHAMMAD RABBANI <rabbanishaikkola9@gmail.com>
2026-05-03 14:40:05 +0530

    Update main.py

--- Rock,paper&scissors/main.py
+++ Rock,paper&scissors/main.py
@@ -41,3 +41,32 @@ else:
         print("You won!!")
     else:
         print("Something went wrong")
+
+'''
+
+import random
+
+computer = random.choice([0, 1, -1])
+
+youdict = {"r": 0, "p": 1, "s": -1}
+reverseddict = {0: "rock", 1: "paper", -1: "scissor"}
+
+youstr = input("Enter choice (r/p/s): ").lower()
+
+if youstr not in youdict:
+    print("Invalid input")
+    exit()
+
+you = youdict[youstr]
+
+print(f"You chose {reverseddict[you]} and computer chose {reverseddict[computer]}")
+
+if you == computer:
+    print("It's a draw")
+elif (you == 0 and computer == -1) or 
+     (you == 1 and computer == 0) or 
+     (you == -1 and computer == 1):
+    print("You win!")
+else:
+    print("Computer wins!")
+'''
